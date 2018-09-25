929bccdd903f1d0b3cab259b7253b333da648f8b
welch298 <welch298@morris.umn.edu>
2018-09-25 11:42:30 -0500

    Make disemvowel.c

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,30 @@
-#include <stdlib.h>
 
+#include <stdlib.h>
+#include <string.h>
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+char* disemvowel(char* string) {
+
+  int i;
+  int j = 0;
+  int len = strlen(string);
+  int count = 0;
+
+  for(i = 0; i < len; ++i){
+    if(!(string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u' || string[i] == 'A' || string[i] == 'E' || string[i] == 'I' || string[i] == 'O' || string[i] == 'U')){
+      ++count;
+    }
+  }
+
+  char* noVow;
+  noVow = calloc(count + 1, sizeof(char));
+
+  for(i = 0; i < len; ++i){
+    if(!(string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u' || string[i] == 'A' || string[i] == 'E' || string[i] == 'I' || string[i] == 'O' || string[i] == 'U')){
+      noVow[j] = string[i];
+      ++j;
+    }
+   }
+
+  return noVow;
 }
