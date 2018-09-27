6f9d5790a36cb39e768cda1c0852cefcafad5314
SirLich <lrkoehler77@gmail.com>
2018-09-26 21:22:15 -0500

    Added comments explaining my solution to the memory leaks

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -10,6 +10,12 @@ int main(int argc, char *argv[]) {
   size = 100;
   line = (char*) malloc (size + 1);
 
+  /*Passing in the result of disemvowel directly into a printf caused lost memory
+  in an impossible to free-way.
+  The "gotcha" one needed to spot was storing disemvowel(line) in a temp-variable.
+
+  Notice that freeing line was also neccesary. 
+  */
   while (getline(&line, &size, stdin) > 0) {
     char *result;
     result = disemvowel(line);
