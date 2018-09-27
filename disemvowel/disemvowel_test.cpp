0b9ad7b2faba50028613d772f17cf3205f1d91c7
SirLich <lrkoehler77@gmail.com>
2018-09-26 20:50:42 -0500

    Reworked the remaining tests to the new standard.
    Still throwing horendeous segmentation-fault errors, but I have faith!

--- disemvowel/disemvowel_test.cpp
+++ disemvowel/disemvowel_test.cpp
@@ -2,41 +2,33 @@
 
 #include "disemvowel.h"
 
-void test_disemvowel(char const *base, char const *expected_result) {
+void test_disemvowel(char *base, char const *expected_result) {
   char *result;
-  result = disemvowel(base)
+  result = disemvowel(base);
   ASSERT_STREQ(result, expected_result);
-  if(strlen(result) >= 0){
+  if(strlen(result) != 0){
     free(result);
   }
 }
 
-// TEST(Disemvowel, HandleEmptyString) {
-//   char *result = disemvowel((char*) "");
-//   ASSERT_STREQ("", result);
-//   free(result);
-// }
+TEST(Disemvowel, HandleEmptyString) {
+  test_disemvowel((char*)"","");
+}
 
 TEST(Disemvowel, HandleNoVowels) {
-  test_disemvowel("pqrst","pqrst");
+  test_disemvowel((char*)"pqrst","pqrst");
 }
 
 TEST(Disemvowel, HandleOnlyVowels) {
-  char *result = disemvowel((char*) "aeiouAEIOUOIEAuoiea");
-  ASSERT_STREQ("", result);
-  free(result);
+  test_disemvowel((char*)"aeiouAEIOUOIEAuoiea","");
 }
 
 TEST(Disemvowel, HandleMorrisMinnesota) {
-  char *result = disemvowel((char*)"Morris, Minnesota");
-  ASSERT_STREQ("Mrrs, Mnnst", result);
-  free(result);
+  test_disemvowel((char*)"Morris, Minnesota","Mrrs, Mnnst");
 }
 
 TEST(Disemvowel, HandlePunctuation) {
-  char *result = disemvowel((char*)"An (Unexplained) Elephant!");
-  ASSERT_STREQ("n (nxplnd) lphnt!", result);
-  free(result);
+  test_disemvowel((char*)"An (Unexplained) Elephant!","n (nxplnd) lphnt!");
 }
 
 TEST(Disemvowel, HandleLongString) {
